e98b14b0e873a209ef191d2eae91bbd8c13ee5cb
guanting <hegting1379@gmail.com>
2024-04-20 21:30:24 -0400

    First commit

---
+++ src/test/java/INF1013/my_restaurant/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package INF1013.my_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
