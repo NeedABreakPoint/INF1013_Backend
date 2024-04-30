5f1b91c6920fe5dcb79be487567933b3c6cc41e8
guanting <hegting1379@gmail.com>
2024-04-29 20:48:45 -0400

    antMatcher problem

--- auth/src/main/java/config/SecurityConfig.java
+++ auth/src/main/java/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import config.jwt.JwtAuthorizationFilter;
 import config.jwt.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -31,19 +33,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .cors().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors()
+                .and()
                 .addFilterAfter(new JwtAuthorizationFilter(configBean, userDetailsService), JwtAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/register", "api/users/register")
+                .antMatchers("/api/users/add")
                 .permitAll()
                 .anyRequest().authenticated();
     }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
